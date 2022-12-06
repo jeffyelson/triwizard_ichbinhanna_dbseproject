b9b5d26ed41cd8efbf328bf02b38a7eb06d4427e
Sunil Ghadwal <maxlab@ovgu.de>
2022-12-06 08:45:10 +0100

    Performed translations on user bios

--- preprocessing/params.py
+++ preprocessing/params.py
@@ -5,7 +5,7 @@ MENTIONS_PATTERN = re.compile(r'@[^ ]+')
 SPECIAL_CHARS_PATTERN = re.compile(r'[^A-Za-zÀ-ž ]')
 MULTIPLE_SPACES_PATTERN = re.compile(' +')
 
-USER_DATA_COLUMNS = ['id', 'created_at', 'updated_bio', 'location', 'public_metrics_followers_count',
+USER_DATA_COLUMNS = ['id', 'created_at', 'description', 'updated_bio', 'translated_bio', 'location', 'public_metrics_followers_count',
                      'public_metrics_following_count', 'public_metrics_tweet_count', 'public_metrics_listed_count',
                      'verified', 'extracted_urls', 'extracted_mentions', 'extracted_hashtags', 'tweets_in_dataset',
                      'retweets_in_dataset']
@@ -14,4 +14,4 @@ TWEET_DATA_COLUMNS = ['id', 'author_id', 'updated_text', 'created_at', 'lang', '
                       'public_metrics_reply_count', 'public_metrics_like_count', 'public_metrics_quote_count',
                       'extracted_urls', 'extracted_mentions', 'extracted_hashtags']
 
-TWEET_USER_MAP_COLUMNS = ['id', 'author_id', 'is_retweet']
+TWEET_USER_MAP_COLUMNS = ['id', 'author_id', 'created_at', 'is_retweet']
--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -1,11 +1,15 @@
 import ast
 import pandas as pd
 import numpy as np
+import time
+from datetime import datetime
+from deep_translator import GoogleTranslator
 import params
 
 
 def preprocess_data(df_tweets, df_users):
     #     Add is_retweet column to tweet data for use in user data
+    print("Adding a retweet flag to the tweets dataset")
     try:
         df_tweets["is_retweet"] = df_tweets["referenced_tweets"].apply(lambda x: is_retweet(x))
     except KeyError:
@@ -22,19 +26,25 @@ def preprocess_data(df_tweets, df_users):
         print('description and/or entities columns not present in df_users')
         return None
 
+    print("Performing text processing on user bios")
     df_users['updated_bio'] = df_users['description'].apply(lambda x: text_process(x,
                                                                                    url_pattern,
                                                                                    mentions_pattern,
                                                                                    special_characters_pattern,
                                                                                    multiple_whitespaces_pattern))
     #     Extract urls, mentions and hashtags from descriptions
+    print("Extracting urls, mentions and hashtags from entities")
     df_users['extracted_urls'] = df_users['entities'].apply(lambda x: extract_urls_from_desc(x))
     df_users['extracted_mentions'] = df_users['entities'].apply(lambda x: extract_mentions_from_desc(x))
     df_users['extracted_hashtags'] = df_users['entities'].apply(lambda x: extract_hashtags_from_desc(x))
 
     #     Update user tweet and retweet from dataset stats
+    print("Extracting user stats from tweets dataset")
     df_users = update_user_tweet_retweet_stats(df_users, df_tweets, tweet_type='og')
     df_users = update_user_tweet_retweet_stats(df_users, df_tweets, tweet_type='re')
+
+    print("Performing bio translations")
+    df_users = translate_text(df_users, params.USER_DATA_COLUMNS[3], params.USER_DATA_COLUMNS[4])
 
     df_users = df_users[params.USER_DATA_COLUMNS]
 
@@ -43,12 +53,15 @@ def preprocess_data(df_tweets, df_users):
         print('text and/or entities columns not present in df_tweets')
         return None
 
+    print("Performing text processing on tweet data")
+
     df_tweets['updated_text'] = df_tweets['text'].apply(lambda x: text_process(x,
                                                                                url_pattern,
                                                                                mentions_pattern,
                                                                                special_characters_pattern,
                                                                                multiple_whitespaces_pattern))
     #     Extract urls, mentions and hashtags from descriptions
+    print("Extracting urls, mentions and hashtags from entities")
     df_tweets['extracted_urls'] = df_tweets['entities'].apply(lambda x: extract_urls_from_tweet(x))
     df_tweets['extracted_mentions'] = df_tweets['entities'].apply(lambda x: extract_mentions_from_tweet(x))
     df_tweets['extracted_hashtags'] = df_tweets['entities'].apply(lambda x: extract_hashtags_from_tweet(x))
@@ -248,3 +261,32 @@ def update_user_tweet_retweet_stats(df_users, df_tweets, tweet_type="og"):
     df_users = df_users.astype({column_name: int})
 
     return df_users
+
+
+def translate_text(df, input_column="text", output_column="translated_text", translator=None):
+    if translator is None:
+        translator = GoogleTranslator(source='auto', target='en')
+
+    if input_column not in df.columns:
+        print('Invalid input column name')
+        return df
+
+    text_list = list(df[input_column].fillna("__BLANK__"))
+    translated_text_list = []
+
+    batch_size = 1000
+
+    for i in range(0, len(text_list), batch_size):
+        time.sleep(1)
+        if (i + batch_size) > len(text_list):
+            temp = text_list[i:len(text_list)]
+        else:
+            temp = text_list[i:i + batch_size]
+        start_time = datetime.now()
+        translated_text_list += translator.translate_batch(temp)
+        end_time = datetime.now()
+        print("Translated " + str(len(temp)) + " texts in - " + str(end_time - start_time))
+
+    df[output_column] = translated_text_list
+
+    return df
