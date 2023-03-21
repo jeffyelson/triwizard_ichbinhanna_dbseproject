01d7d26ebdb9fc2a98cb2cc8dd093420e039d1c3
Preetam Vinod Naik <preetam.naik@st.ovgu.de>
2023-03-21 14:56:32 +0000

    VIsualisation

---
+++ StreamLit/Page1.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+st.set_page_config(
+    page_title="Twitter Data Anakysis"
+)
+
+st.sidebar.success("Select a page above.")
+st.header("Our final Cleaned dataset for Users with Classfication:")
+df = pd.read_csv('Final_Users_File.csv')
+st.dataframe(df)
+
+
+
+st.header("Count of different user groups present in our dataset")
+
+bar = df["user_category"].value_counts().rename_axis('user_categories').reset_index(name='counts')
+fig = px.bar(bar, x='user_categories', y='counts',color_discrete_sequence=px.colors.sequential.Viridis)
+st.plotly_chart(fig)
+
+st.header("Top 15 countries present in our dataset")
+bar1 = df["location_country"].value_counts().rename_axis('location').reset_index(name='counts')
+s= bar1.head(15)
+fig1 = px.bar(s, y='location', x='counts',color_discrete_sequence=px.colors.sequential.Viridis)
+
+fig1.update_layout(yaxis=dict(autorange="reversed"))
+
+
+st.plotly_chart(fig1)
+
+
